0333d2029a810d93823419dc9aa738faf48455a9
RutamBhagat <rutambhagat@gmail.com>
2026-07-10 17:37:51 +0530

    feat: add health route

---
+++ src/pages/api/[[...path]].ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { app } from "@/server/app";
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  app(req, res);
+}
